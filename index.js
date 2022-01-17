4c4d53cc5a13a538fd77a23fc3f5c339ee4fc94b
web-dev <nathankim137311@gmail.com>
2022-01-16 18:51:11 -0800

    completed project

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const port = 3000;
 app.listen(port, () => console.log(`server started on port ${port}...`));
 
 // Middleware 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname)); // css as static
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/new', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/new', (req, res) => {
-    res.send(req.body);
+app.post('/messages', (req, res) => {
+    const message = {
+        title: req.body.title,
+        message: req.body.message,
+        date: new Date(), 
+    }
+    res.send(`<a href="/new">New Message</a><p>${message.title}</p><p>${message.message}</p><p>${message.date}</p>`).status(200);
 });
-
